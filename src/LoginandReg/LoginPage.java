b76685fc412f554b778c7bf135835699217fa395
Aayush <joshiaayush871@gmail.com>
2021-08-24 16:48:50 +0545

    commenting

--- src/LoginandReg/LoginPage.java
+++ src/LoginandReg/LoginPage.java
@@ -25,6 +25,7 @@ public class LoginPage {
         p = new JPanel();
 
 
+        //Login Credential
         JLabel Heading= new JLabel("Login Credential");
         Heading.setBounds(200,20,600,60);
         Heading.setFont(font1);
@@ -32,44 +33,49 @@ public class LoginPage {
         p.add(Heading);
 
 
+        //User Icon
         JLabel UserIcon;
         ImageIcon user = new ImageIcon("resource/user.png");
         UserIcon = new JLabel("",user,JLabel.CENTER);
         UserIcon.setBounds(190,130,80,80);
         p.add(UserIcon);
 
+        //username
         JLabel UserName = new JLabel("User Name");
         UserName.setFont(font2);
         UserName.setBounds(205,205,200,30);
         UserName.setForeground(Color.WHITE);
         p.add(UserName);
 
+        //username text field
         JTextField UName = new JTextField();
         UName.setBounds(290,155,250,30);
         UName.setFont(font3);
         UName.setBackground(Color.lightGray);
         p.add(UName);
 
-
+        //user password icon
         JLabel UserPass;
         ImageIcon pass = new ImageIcon("resource/pass.png");
         UserPass = new JLabel("",pass,JLabel.CENTER);
         UserPass.setBounds(190,240,80,80);
         p.add(UserPass);
 
+        //user password label
         JLabel Password = new JLabel("Password");
         Password.setFont(font2);
         Password.setBounds(205,315,200,30);
         Password.setForeground(Color.WHITE);
         p.add(Password);
 
+        //user password text field
         JPasswordField password = new JPasswordField();
         password.setBounds(290,260,250,30);
         password.setFont(font3);
         password.setBackground(Color.lightGray);
         p.add(password);
 
-
+        //Login button
         JButton Login = new JButton("Login");
         Login.setBackground(Color.GREEN);
         Login.setForeground(Color.BLACK);
@@ -77,6 +83,7 @@ public class LoginPage {
         Login.setFont(font4);
         p.add(Login);
 
+        //Reset button
         JButton Reset = new JButton("Reset");
         Reset.setBackground(Color.ORANGE);
         Reset.setForeground(Color.BLACK);
@@ -85,12 +92,14 @@ public class LoginPage {
         p.add(Reset);
 
 
+        //signup label
         JLabel Account = new JLabel("Create New Account:");
         Account.setFont(font3);
         Account.setBounds(450,500,200,30);
         Account.setForeground(Color.CYAN);
         p.add(Account);
 
+        //signup button
         JButton SignUp = new JButton("Sign Up");
         SignUp.setBackground(Color.cyan);
         SignUp.setForeground(Color.BLACK);
@@ -103,9 +112,6 @@ public class LoginPage {
         image = new ImageIcon("resource/background.jpg");
         background = new JLabel("",image,JLabel.CENTER);
         background.setBounds(0,0,800,600);
-
-
-
 
 
         p.setBounds(0,0,800,600);
