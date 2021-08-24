2c9000bf1c5f2e4c5249c34b7577e737999df265
Aayush <joshiaayush871@gmail.com>
2021-08-24 12:51:50 +0545

    Adding Jpanel

--- src/LoginandReg/LoginPage.java
+++ src/LoginandReg/LoginPage.java
@@ -1,12 +1,35 @@
 package LoginandReg;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginPage {
     JFrame f;
+    Font font1,font2,font3;
+    JPanel p;
+
     LoginPage(){
-        f = new JFrame();
-        JLabel Heading= new JLabel("Login Page");
+        f = new JFrame("Login Page");
+
+
+        p = new JPanel();
+        p.setBounds(0,0,800,600);
+        p.setBackground(Color.gray);
+        p.setLayout(null);
+        p.setVisible(true);
+        f.add(p);
+
+        font1 = new Font("Cambria",Font.BOLD,50);
+
+
+
+
+        JLabel Heading= new JLabel("Login Credential");
+        Heading.setBounds(200,20,600,60);
+        Heading.setFont(font1);
+        p.add(Heading);
+
+        JLabel UserName = new JLabel("User Name");
 
 
 
